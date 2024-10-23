65e1e4f7b0bb701be9f7b3f23aa3614d145cdb3b
mochi <95735459+KOOHIIii@users.noreply.github.com>
2024-10-24 03:03:51 +0700

    add Ninjamail Todolist Colorpicker to Selfproject page

--- src/components/SelfProject.js
+++ src/components/SelfProject.js
@@ -9,7 +9,7 @@ export default function SelfProject(){
         }
     }
     function next(){
-        if (navSelf < 2){
+        if (navSelf < 3){
             setNavself(navSelf = navSelf+1)
         }
     }
@@ -17,16 +17,54 @@ export default function SelfProject(){
         <div className='self-top-item'>
             <div className={`bullet ${navSelf === 1 && "active"}`}></div>
             <div className={`bullet ${navSelf === 2 && "active"}`}></div>
+            <div className={`bullet ${navSelf === 3 && "active"}`}></div>
         </div>
         <div className='self-left-item'>
             <i onClick={previous} class="fa-solid fa-chevron-left"></i>
         </div>
         <div className='self-center-item'>
-            {navSelf === 1 ? <letter/> : null}
-            {navSelf === 2 ? <speexx/> : null}
+            {navSelf === 1 ? <NinjaMail/> : null}
+            {navSelf === 2 ? <Todolist/> : null}
+            {navSelf === 3 ? <Colorpick/> : null}
         </div>
         <div className='self-right-item'>
             <i onClick={next} class="fa-solid fa-chevron-right"></i>
+        </div>
+    </div>
+}
+
+function NinjaMail(){
+    return <div className='ninja-wrap-all'>
+        <div className='ninja-wrap-link'>
+            <h2>Ninja Mail - Layout and Responsive Design Practice -</h2>
+            <a href='https://koohiiii.github.io/Ninja-Mail/'><h2>Visit Site</h2></a>
+        </div>
+        <div className='ninja-img-box'>
+            <img src='/img/ninjamail.png' alt='ninja mail' />
+        </div>
+    </div>
+}
+
+function Todolist(){
+    return <div className='todo-wrap-all'>
+        <div className='todo-wrap-link'>
+            <h2>ToDoList - Javascript and DOM Practice -</h2>
+            <a href='https://koohiiii.github.io/Project-To-Do-List/'><h2>Visit Site</h2></a>
+        </div>
+        <div className='todo-img-box'>
+            <img src='/img/todolist.png' alt='todolist' />
+        </div>
+    </div>
+}
+
+function Colorpick(){
+    return <div className='color-wrap-all'>
+        <div className='color-wrap-link'>
+            <h2>Color Picker - Javascript and DOM Practice -</h2>
+            <a href='https://koohiiii.github.io/Project-Color-Picker/'><h2>Visit Site</h2></a>
+        </div>
+        <div className='color-img-box'>
+            <img src='/img/colorpick.png' alt='colorpicker' />
         </div>
     </div>
 }
