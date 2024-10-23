2fe8910fc291bab95fe12fb1328f053d92e129de
mochi <95735459+KOOHIIii@users.noreply.github.com>
2024-10-23 15:48:26 +0700

    set up layout of Certificate page

---
+++ src/components/Certificate.js
@@ -0,0 +1,7 @@
+import './Certificate.css'
+
+export default function Certificate(){
+    return <div>
+        <h1 className='cer-h1'>Certificate Page</h1>
+    </div>
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import './Home.css';
 export default function Home(){
     return <div>
-        <h1>Home page</h1>
+        <h1 className='home-h1'>Home page</h1>
     </div>
 }
